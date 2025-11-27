988b3a97517b45b35f57882ae28b9f3b3eab2f07
Khoa Dam <damngockhoa0703@gmail.com>
2025-11-27 18:38:06 +0700

    Add url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import { env } from '@/lib/env'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = env.NEXT_PUBLIC_APP_URL || 'https://guard-m-fe.vercel.app/'
+  const baseUrl = env.NEXT_PUBLIC_APP_URL || 'https://www.guardm.space/'
 
   return {
     rules: {
